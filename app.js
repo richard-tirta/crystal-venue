a41260e926d019307c26ee9d3d3ab070de4ca06c
Richard T. Widjaja <rt@richardtirta.com>
2021-03-11 16:18:57 -0800

    update

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
-const app = express();
 
-var gallery = require('./gallery');
+const DISCORD_CLIENT_ID = '819420216583913532';
+const DISCORD_CLIENT_SECRET = 'whvp1V0pnXoMsm6O1zFqAQ4l8IDvBMB5';
+
+const app = express();
+const fetch = require('node-fetch');
+
+const DiscordOauth2 = require("discord-oauth2");
+const oauth = new DiscordOauth2();
+
 
 const port = process.env.PORT || 3000;
 
@@ -28,8 +35,54 @@ app.get('/venue', function (req, res) {
 });
 
 app.get('/profile', function (req, res) {
+	console.log('GET request at profile made', req.query)
 	res.redirect('/profile.html');
 	// res.status(200).send({ success: true })
+	fetch('https://discord.com/api/oauth2/token', {
+				method: 'GET',
+				mode: 'cors',
+                data: {
+                    client_id: DISCORD_CLIENT_ID,
+                    client_secret: DISCORD_CLIENT_SECRET,
+                    code: req.query.code,
+                    scope: "identify guilds",
+                    grantType: "authorization_code",
+                    redirectUri: "https://dry-badlands-14718.herokuapp.com/profile"
+                },
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				}
+	})
+		.then(
+			function (response) {
+				if (response.status !== 200) {
+					console.log('Looks like there was a problem. Status Code: ' + response.status);
+                    return;
+				}
+				response.json().then(function (data) {
+					console.log('horray', data);
+				});
+			}
+		)
+        .catch(err => console.log(err));
+});
+
+
+
+//https://discord.com/api/oauth2/token
+
+app.get('/discord', function (req, res) { 
+	console.log('POST request for Discord made', req.query.code);
+	oauth.tokenRequest({
+		clientId: DISCORD_CLIENT_ID,
+		clientSecret: DISCORD_CLIENT_SECRET,
+	 
+		code: req.query.code,
+		scope: "identify guilds",
+		grantType: "authorization_code",
+		
+		redirectUri: "https://dry-badlands-14718.herokuapp.com/profile",
+	}).then(console.log)
 });
 
 app.get('/about', function (req, res) {
@@ -42,7 +95,7 @@ const { body } = require('express-validator/check');
 
 
 //The 404 Route (ALWAYS Keep this as the last route)
-app.use(function (req, res, next) {
-	res.status(404).redirect('/404.html');
-	console.log('Sending 404, this was requested:', req.url);
-})
+// app.use(function (req, res, next) {
+// 	res.status(404).redirect('/404.html');
+// 	console.log('Sending 404, this was requested:', req.url);
+// })
--- src/assets/ProfileForm.js
+++ src/assets/ProfileForm.js
@@ -13,6 +13,58 @@ class ProfileForm extends React.Component {
 
     }
 
+    componentDidMount() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const myParam = urlParams.get('code');
+        const fragment = new URLSearchParams(window.location.hash.slice(1));
+        const accessToken = fragment.get("access_token");
+        const tokenType = fragment.get("token_type");
+        const DISCORD_CLIENT_ID = '819420216583913532';
+        const DISCORD_CLIENT_SECRET = 'whvp1V0pnXoMsm6O1zFqAQ4l8IDvBMB5';
+        console.log(myParam, accessToken, tokenType);
+
+        if (myParam) {
+            fetch('/discord?code=' + myParam)
+                .then(response => response.json())
+                .then(
+                    (result) => {
+                        console.log(result);
+                    },
+                    (error) => {
+                        console.log('error');
+                    }
+            )
+
+            //https://discord.com/api/oauth2/authorize?client_id=819420216583913532&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fprofile&response_type=code&scope=identify%20guilds
+
+            // {
+            //     headers: new Headers({
+            //         client_id: DISCORD_CLIENT_ID,
+            //         client_secret: DISCORD_CLIENT_SECRET,
+            //         code: myParam,
+            //         scope: "identify guilds",
+            //         grantType: "authorization_code",
+            //         redirectUri: "https:localhost:3000/profile"
+            //     })
+            // }
+
+            // fetch('https://discord.com/api/oauth2/token', {
+            //     headers: {
+            //         client_id: DISCORD_CLIENT_ID,
+            //         client_secret: DISCORD_CLIENT_SECRET,
+            //         code: myParam,
+            //         scope: "identify guilds",
+            //         grantType: "authorization_code",
+            //         redirectUri: "https:localhost:3000/profile"
+            //     }
+            //     })
+            //     .then(response => response.json()) 
+            //     .then(json => console.log(json))
+            //     .catch(err => console.log(err));
+
+        }
+    }
+
     render() {
         return (<div>
         </div>);
