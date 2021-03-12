f5a9c4da60a98f8f5090d9f213463ada25826292
Richard T. Widjaja <rt@richardtirta.com>
2021-03-12 12:26:59 -0800

    progress

--- discord.js
+++ discord.js
@@ -15,20 +15,24 @@ exports.init = function (req, res) {
 	app.use(cookieParser());
 	app.use(bodyParser.json())
 	app.use(cors())
-	
+
 	// id: '797508055960256554',
 	// name: 'Eagle Dragon Leather Bar',
 	//id: '758143077701124137',
 	// name: 'Crystal Venue Association',
 
 	const getAppCookies = (req) => {
-		const rawCookies = req.headers.cookie.split('; ');
-		const parsedCookies = {};
-		rawCookies.forEach(rawCookie=>{
-		const parsedCookie = rawCookie.split('=');
-		 parsedCookies[parsedCookie[0]] = parsedCookie[1];
-		});
-		return parsedCookies;
+		if (req.headers.cookie) {
+			const rawCookies = req.headers.cookie.split('; ');
+			const parsedCookies = {};
+			rawCookies.forEach(rawCookie => {
+				const parsedCookie = rawCookie.split('=');
+				parsedCookies[parsedCookie[0]] = parsedCookie[1];
+			});
+			return parsedCookies;
+		} else {
+			return undefined;
+		}
 	};
 
 	app.get('/profile', function (req, res) {
@@ -56,7 +60,7 @@ exports.init = function (req, res) {
 				headers: {
 					authorization: `${token_type} ${token}`,
 				}
-				
+
 			})
 				.then(
 					function (response) {
@@ -87,7 +91,7 @@ exports.init = function (req, res) {
 											maxAge: 24 * 60 * 60,
 											httpOnly: true
 										});
-										
+
 									}
 								});
 							}
@@ -98,36 +102,40 @@ exports.init = function (req, res) {
 				.catch(err => console.log(err));
 		}
 
-		if (req.query.code) {
-			const data = {
-				client_id: DISCORD_CLIENT_ID,
-				client_secret: DISCORD_CLIENT_SECRET,
-				grant_type: "authorization_code",
-				code: req.query.code,
-				redirect_uri: "http://localhost:3000/profile",
-				scope: "identify guilds"
-			};
-
-			fetch('https://discord.com/api/oauth2/token', {
-				method: 'POST',
-				body: new URLSearchParams(data),
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded;'
-				}
-					
-			})
-			.then(
-				function (response) {
-					response.json().then(function (data) {
-						getUser(data.access_token, data.token_type);
-					});
-				}
-			)
-				.catch(err => console.log(err));
-			
+		if (getAppCookies(req, res)) {
+			res.redirect('/profile.html');
 		} else {
-			res.redirect('https://discord.com/api/oauth2/authorize?client_id=819420216583913532&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fprofile&response_type=code&scope=identify%20guilds');
-		}		
+			if (req.query.code) {
+				const data = {
+					client_id: DISCORD_CLIENT_ID,
+					client_secret: DISCORD_CLIENT_SECRET,
+					grant_type: "authorization_code",
+					code: req.query.code,
+					redirect_uri: "http://localhost:3000/profile",
+					scope: "identify guilds"
+				};
+
+				fetch('https://discord.com/api/oauth2/token', {
+					method: 'POST',
+					body: new URLSearchParams(data),
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded;'
+					}
+
+				})
+					.then(
+						function (response) {
+							response.json().then(function (data) {
+								getUser(data.access_token, data.token_type);
+							});
+						}
+					)
+					.catch(err => console.log(err));
+
+			} else {
+				res.redirect('https://discord.com/api/oauth2/authorize?client_id=819420216583913532&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fprofile&response_type=code&scope=identify%20guilds');
+			}
+		}
 	});
 
 	app.get('/discord', (req, res) => {
@@ -135,5 +143,5 @@ exports.init = function (req, res) {
 		userInfo = dbCache.find(user => parseInt(user.id) == userId);
 		res.send(userInfo);
 	});
-	
+
 }
