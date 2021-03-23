c58ba535ef03ba9a514665195d7c43f5aa2e141f
Richard T. Widjaja <rt@richardtirta.com>
2021-03-23 15:18:30 -0700

    bugfix

--- listings.js
+++ listings.js
@@ -135,8 +135,8 @@ exports.init = function (req, res) {
 			}
 		}
 
-		if (userId) {
-			getUserByUserId(userId).then((userInfo) => {
+		if (userIdAuth) {
+			getUserByUserId(userIdAuth).then((userInfo) => {
 				sessionData.userData.userName = userInfo[0].username;
 				sessionData.userData.isUserMature = getAge(userInfo[0].birthday) > 18 ? true : false;
 				sendData();
