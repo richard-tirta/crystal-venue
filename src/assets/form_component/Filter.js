fab6f056faaa5e30d819210d8e242a552733be28
Richard T. Widjaja <rt@richardtirta.com>
2021-03-25 19:21:55 -0700

    responsive styles

--- src/assets/form_component/Filter.js
+++ src/assets/form_component/Filter.js
@@ -6,24 +6,24 @@ const EventTime = ((props) => (
         <div className="listing-filters">
             <div className="main-filters">
                 <div className="form-checkbox-container">
-                    <label htmlFor="music">Live Music</label>
                     <input className="form-checkbox" type="checkbox" id="filter-music" name="filterMusic" onChange={props.onChange} />
+                    <label htmlFor="music">Live Music</label>
                 </div>
                 <div className="form-checkbox-container">
-                    <label htmlFor="full-bar">Full Bar</label>
                     <input className="form-checkbox" type="checkbox" id="filter-bar" name="filterFullBar" onChange={props.onChange} />
+                    <label htmlFor="full-bar">Full Bar</label>
                 </div>
                 <div className="form-checkbox-container">
-                    <label htmlFor="food">Food</label>
                     <input className="form-checkbox" type="checkbox" id="filter-food" name="filterFood" onChange={props.onChange} />
+                    <label htmlFor="food">Food</label>
                 </div>
                 <div className="form-checkbox-container">
-                    <label htmlFor="novelties">Novelties</label>
                     <input className="form-checkbox" type="checkbox" id="filter-novelties" name="filterNovelties" onChange={props.onChange} />
+                    <label htmlFor="novelties">Novelties</label>
                 </div>
                 <div className="form-checkbox-container">
-                    <label htmlFor="lgbtq-owned">LGBTQ+ Owned</label>
                     <input className="form-checkbox" type="checkbox" id="filter-lgbtq" name="filterLgbtq" onChange={props.onChange} />
+                    <label htmlFor="lgbtq-owned">LGBTQ+ Owned</label>
                 </div>
             </div>
             <div className="secondary-filters">
